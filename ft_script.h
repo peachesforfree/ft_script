8db937a31833914baba4b151c6e70ec0c8b21bbf
Sergio Balcorta <sergio.shearing@gmail.com>
2018-04-18 13:56:06 -0700

    added in library and all flags are working

--- ft_script.h
+++ ft_script.h
@@ -7,7 +7,11 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <sys/types.h>
-# define FLAGS  "adkpqr"
+# include <sys/stat.h>
+# include "libft/libft.h"
+
+
+# define FLAGS  "adkpqrft"
 # define FLAGDASH   '-'
 # define AFLG   0b00000001
 # define DFLG   0b00000010
@@ -17,7 +21,8 @@
 # define RFLG   0b00100000
 # define FFLG   0b01000000
 # define TFLG   0b10000000
-
+# define DEFAULTFILE    "typescript"
+/*
 char **lex[5] = {
     "ft_script",
     "-adkpqr",
@@ -26,14 +31,15 @@ char **lex[5] = {
     "file [command ...]",
     NULL
 };
+*/
 
-typedef struct  s_env
+typedef struct          s_env
 {
-    char        *pipe;
-    char        *time;
-    char        **file;
-}               t_env;
+    char                *pipe;
+    int                 time;
+    char                *file;
+    unsigned char       flags;
+}                       t_env;
 
-char    ft_get_flags(int count, char **input, char *flags);
 
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,56 @@
 
 #include "ft_script.h"
 
+/*
+**Checks if there is another argv in line and checks if its a file. 
+**if so, assigned to char **name
+*/
+int    get_file_name(int ac, int index, char **av, char **name)
+{
+    struct stat     *statbuf;
+    
+    if (index < ac)
+    {
+        *name = ft_strdup(av[index]);
+        return (1);
+    }
+    return (0);
+}
+
+int     get_number(int count, int index, char **input, t_env *env)
+{
+    int     ret;
+
+    if ((index + 1) < count)
+    {
+        if (ft_isdigit((int)input[index + 1]))
+            env->time = ft_atoi(input[index + 1]);
+        else    //time value not dicated. default 30
+            env->time = 30;
+        return (1);
+    }
+    env->time = 30;
+    return (0);
+}
+
+/*
+**Retrieves flags 
+**stored as bits within env->
+*/
 char    get_flags(int count, char **input, t_env *env)
 {
     unsigned char   flg;
     int             index;
     int             letter;
 
-    letter = 0;
     index = 0;
     flg = 0;
     while (index < count)
     {
         if (input[index][0] == FLAGDASH)
-            while (ft_strchr(FLAGS, input[index][letter]))
+        {
+            letter = 1;
+            while (input[index][letter] && ft_strchr(FLAGS, input[index][letter]))
             {
                 (input[index][letter] == 'a') ? (flg |= AFLG ) : (0);
                 (input[index][letter] == 'd') ? (flg |= DFLG ) : (0);
@@ -21,26 +58,45 @@ char    get_flags(int count, char **input, t_env *env)
                 (input[index][letter] == 'p') ? (flg |= PFLG ) : (0);
                 (input[index][letter] == 'q') ? (flg |= QFLG ) : (0);
                 (input[index][letter] == 'r') ? (flg |= RFLG ) : (0);
-                (input[index][letter] == 'f') ? (flg |= FFLG ) : (0);
-                if (input[index][letter] == 'f')
-                    get_file_name();
-                (input[index][letter] == 't') ? (flg |= TFLG ) : (0);
+                //(input[index][letter] == 'f') ? (flg |= FFLG ) : (0);
+                if (input[index][letter] == 'f')                        //makes a named pipe ? using mkfifo(1)
+                {
+                    flg |= FFLG;
+                    index += get_file_name(count, index + 1, input, &env->pipe);
+                }
+                (input[index][letter] == 't') ? (flg |= TFLG ) : (0); //default interval 30 seconds 
                 if (input[index][letter] == 't')
-                    get_number();                
+                        index += get_number(count, index, input, env);
                 letter++;
             }
-            if (index == (count - 1))
-                get_file_nme();
+        }
+        //if (index == (count - 1))
+        //    index += get_file_name(count, index, input, &env->file);
+        if (index == (count - 1) 
+        && env->pipe != NULL 
+        && ft_strcmp(env->pipe, input[index]))
+            get_file_name(count, index, input, &env->file);
         index++;
     }
+    if (env->file == NULL)  //assigning standard file name 'typescript'
+        env->file = ft_strdup(DEFAULTFILE);
     return (flg);
+}
+
+void    print_flags(t_env *env)
+{
+    printf("env->pipe='%s'\n", env->pipe);
+    printf("env->time='%d'\n", env->time);
+    printf("env->file='%s'\n", env->file);
+    printf("env->flags='%d'\n", (unsigned short)env->flags);
 }
 
 int main(int argc, char **argv)
 {
-    unsigned char   flags;
     t_env           env;
-    flags = get_flags(argc, argv, env);
-    
+
+    env.flags = get_flags(argc, argv, &env);
+    //testing
+    print_flags(&env);
     return(0);
 }
