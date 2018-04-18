c50ea5da8587d57c62cdf2323f5cc480e4edea39
Sergio Balcorta <sergio.shearing@gmail.com>
2018-04-17 22:17:27 -0700

    starting to parse flags

--- ft_script.h
+++ ft_script.h
@@ -7,7 +7,33 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <sys/types.h>
+# define FLAGS  "adkpqr"
+# define FLAGDASH   '-'
+# define AFLG   0b00000001
+# define DFLG   0b00000010
+# define KFLG   0b00000100
+# define PFLG   0b00001000
+# define QFLG   0b00010000
+# define RFLG   0b00100000
+# define FFLG   0b01000000
+# define TFLG   0b10000000
 
+char **lex[5] = {
+    "ft_script",
+    "-adkpqr",
+    "-F pipe",
+    "-t time",
+    "file [command ...]",
+    NULL
+};
 
+typedef struct  s_env
+{
+    char        *pipe;
+    char        *time;
+    char        **file;
+}               t_env;
+
+char    ft_get_flags(int count, char **input, char *flags);
 
 #endif
--- main.c
+++ main.c
@@ -1,15 +1,46 @@
 
 #include "ft_script.h"
 
-int main(void)
+char    get_flags(int count, char **input, t_env *env)
 {
-    int i;
+    unsigned char   flg;
+    int             index;
+    int             letter;
 
-    i = 0;
-    printf("getpid(%d)\n", getpid());
-    printf("getppid(%d)\n", getppid());
-    fork();
-    while (1)
-        i++;
+    letter = 0;
+    index = 0;
+    flg = 0;
+    while (index < count)
+    {
+        if (input[index][0] == FLAGDASH)
+            while (ft_strchr(FLAGS, input[index][letter]))
+            {
+                (input[index][letter] == 'a') ? (flg |= AFLG ) : (0);
+                (input[index][letter] == 'd') ? (flg |= DFLG ) : (0);
+                (input[index][letter] == 'k') ? (flg |= KFLG ) : (0);
+                (input[index][letter] == 'p') ? (flg |= PFLG ) : (0);
+                (input[index][letter] == 'q') ? (flg |= QFLG ) : (0);
+                (input[index][letter] == 'r') ? (flg |= RFLG ) : (0);
+                (input[index][letter] == 'f') ? (flg |= FFLG ) : (0);
+                if (input[index][letter] == 'f')
+                    get_file_name();
+                (input[index][letter] == 't') ? (flg |= TFLG ) : (0);
+                if (input[index][letter] == 't')
+                    get_number();                
+                letter++;
+            }
+            if (index == (count - 1))
+                get_file_nme();
+        index++;
+    }
+    return (flg);
+}
+
+int main(int argc, char **argv)
+{
+    unsigned char   flags;
+    t_env           env;
+    flags = get_flags(argc, argv, env);
+    
     return(0);
 }
